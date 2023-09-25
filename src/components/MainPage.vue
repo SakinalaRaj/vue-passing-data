b5d7a05dfe2a28076642265097075803b8a96511
SakinalaRaj <143786644+SakinalaRaj@users.noreply.github.com>
2023-09-25 19:16:43 +0530

    first commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="showPopup = true">Show modal</button>
+    <SubPage v-show="showPopup" @close="closePopup" />
+  </div>
+</template>
+
+<script>
+import SubPage from "./SubPage.vue";
+export default {
+  name: "MainPage",
+  components: { SubPage },
+  data() {
+    return { showPopup: false };
+  },
+  methods: {
+    closePopup(name) {
+      (this.showPopup = false), console.log("name", name);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/SubPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>This is Sub Page</h3>
+    <button @click="$emit('close', 'raju')">Hide this Subpage</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubPage",
+  emits: ["close"],
+};
+</script>
+
+<style scoped></style>
